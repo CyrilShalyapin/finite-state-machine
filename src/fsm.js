767e66e19e893ca38ab340e8e89aafeb1e774c7b
Cyril Shalyapin <cyrilshalyapin@gmail.com>
2017-02-03 13:53:24 +0300

    finite-state-machine/fix: did get rid of hardcore coding, algorithms one love

--- src/fsm.js
+++ src/fsm.js
@@ -1,38 +1,13 @@
-const config = {
-    initial: 'normal',
-    states: {
-        normal: {
-            transitions: {
-                study: 'busy',
-            }
-        },
-        busy: {
-            transitions: {
-                get_tired: 'sleeping',
-                get_hungry: 'hungry',
-            }
-        },
-        hungry: {
-            transitions: {
-                eat: 'normal'
-            },
-        },
-        sleeping: {
-            transitions: {
-                get_hungry: 'hungry',
-                get_up: 'normal',
-            },
-        },
-    }
-};
-
 class FSM {
     constructor(config) {
-    	this.length = 0;
-    	this.nameList = [config.initial];
-    	this.next = config.states.normal;
-    	this.prev = [null];
-        this.nextLost = null;
+        if (config) {
+            this.config = config;
+            this.length = 0;
+            this.nameList = [this.config.initial];
+            this.prev = [null];
+        } else {
+            throw new Error("config unobtainable!")
+        }
     	
     }
 
@@ -41,103 +16,58 @@ class FSM {
     }
 
     changeState(state) {
-    	for (let key in config.states) {
+    	for (let key in this.config.states) {
     		if (key === state) {
-                while (this.length < (this.nameList.length-1)) {
+                 while (this.length < (this.nameList.length-1)){
                     this.prev.pop();
                     this.nameList.pop();
                 }
-    			this.prev.push(this.next);
-    			this.length++;
-    			switch (state) {
-					case 'hungry':
-						this.next = config.states.hungry;
-						this.nameList.push(state);
-						break;
-					case 'normal':
-						this.next = config.states.normal;
-						this.nameList.push(state);
-						break;
-					case 'busy':
-						this.next = config.states.busy;
-						this.nameList.push(state);
-						break;
-					case 'sleeping':
-						this.next = config.states.sleeping;
-						this.nameList.push(state);
-						break;
-    			}
+                this.prev.push(this.nameList[this.length]);
+                this.nameList.push(state);
+                this.length++;
     			return;
     		}
  		}
- 		throw new Error('improper nameList of state!')    	
+ 		throw new Error('improper name of state!')    	
     }
 
     trigger(event) {
-    	for (let key in this.next.transitions) {
+    	for (let key in this.config.states[this.getState()].transitions) {
     		if (key === event) {
-                while (this.length < (this.nameList.length-1)) {
-                    this.prev.pop();
-                    this.nameList.pop();
-                }
-    			this.prev.push(this.next);
-    			this.length++;
-    			switch (event) {
-    				case 'study':
-						this.next = config.states.busy;
-						this.nameList.push('busy');
-						break;
-    				case 'get_tired':
-						this.next = config.states.sleeping;
-						this.nameList.push('sleeping');
-						break;
-    				case 'get_hungry':
-						this.next = config.states.hungry;
-						this.nameList.push('hungry');
-						break;
-    				case 'eat':
-    				case 'get_up':
-						this.next = config.states.normal;
-						this.nameList.push('normal');
-						break;
-    			}
+                let nextState = this.config.states[this.getState()].transitions[key];
+                this.changeState(nextState);
     			return;
     		}
     	}
-    	throw new Error('improper nameList of event!');
+    	throw new Error('improper name of event!');
     }
 
     reset() {
     	this.length = 0;
-        this.nextLost = null;
-    	this.nameList = [config.initial];
-    	this.next = config.states.normal;
+    	this.nameList = [this.config.initial];
     	this.prev = [null];
     }
 
     getStates(event) {
-    	switch (event) {
-    		case undefined:
-    			return ['normal', 'busy', 'hungry', 'sleeping'];
-    		case 'study':
-    			return ['normal'];
-    		case 'get_hungry':
-    			return ['busy', 'sleeping'];
-    		case 'get_tired':
-    			return ['busy'];
-    		case 'get_up':
-    			return ['sleeping'];
-    		case 'eat':
-    			return ['hungry'];
-    		default:
-    			return [];
-    	}
+        let box = [];
+    	if (event === undefined) {
+            for (let key in this.config.states) {
+                box.push(key);
+            }
+        } else {
+            for (let keyStates in this.config.states) {
+                for (let keyEvents in this.config.states[keyStates].transitions) {
+                    if (keyEvents === event) {
+                        box.push(keyStates);
+                    }
+                }
+            }
+        }
+        return box;
     }
 
     undo() {
-    	if (this.prev[this.length] === null) return false;
-        this.nextLost = this.next;
-    	this.next = this.prev[this.length];
+    	if (this.length === 0) return false;
     	this.length--;
     	return true;
 
@@ -145,21 +75,16 @@ class FSM {
     }
 
     redo() {
-        if (this.prev.length === 1 || this.length === (this.nameList.length-1)) {
+        if (this.nameList.length === 1 || this.length === (this.nameList.length-1)) {
             return false;
         } 
         this.length++;
-        if (this.length === (this.nameList.length-1)) {
-            this.next = this.nextLost;
-        } else {
-            this.next = this.prev[this.length];
-        }
         return true;
     }
 
     clearHistory() {
-        this.nameList.splice(1, this.length);
-        this.prev.splice(1, this.length);
+        this.nameList.splice(0, this.length);
+        this.prev.splice(0, this.length);
         this.length = 0;
     }
 }
